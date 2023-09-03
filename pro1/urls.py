175a7ee2b0b4fa593e9824040e6966d373a3fb1b
Huzaifa <Huzuser@gmail.com>
2023-09-03 18:39:21 +0530

    Final Commit

--- pro1/urls.py
+++ pro1/urls.py
@@ -36,3 +36,5 @@ urlpatterns = [
     path("order",views.order, name='order'),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
