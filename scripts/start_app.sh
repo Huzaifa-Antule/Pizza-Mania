f2f1b8d191777fd7dd480247072d5faadf8ba71f
Huzaifa <Huzuser@gmail.com>
2023-09-05 19:00:20 +0530

    commiting

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/Pizza-Mania/pro1/settings.py
+sed -i 's/\[]/\["15.207.14.235"]/' /home/ubuntu/Pizza-Mania/pro1/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
