c33f3a058bf9ff1f70669788fd227a4bcf0fed2c
Huzaifa Antule <120238440+Huzaifa-Antule@users.noreply.github.com>
2023-09-05 16:29:04 +0530

    Update stop_app.sh

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,15 +1,1 @@
-# Import necessary Django modules
-import os
-import django
-from django.contrib.auth.models import User
 
-# Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro1.settings')
-django.setup()
-
-# Create a superuser
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'Huzaifa@1')
-    print('Superuser created successfully!')
-else:
-    print('Superuser already exists.')
